2ab36a06360fc1fe74a2b50f026cf35314c0dc28
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-28 12:39:58 +0200

    milestone 0 + 1
    add base call to API,
    add input and button to search for a movie,
    return movie title and other details,

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <script >
-
+import { state } from './state.js';
 
 export default{
    name: "App",
+   data (){
+      return {
+         state
+      }
+   },
+   created(){
+      state.fetchData()
+   }
 }
 </script>
 
 <template>
 
-   <div class="text-warning">Test</div>
+   <input type="text" placeholder="Search a movie" v-model="state.inputSearchMovie">
+   <button @click="state.fetchData()">SEARCH</button>
+
+   <ul>
+      <li v-for="movie in state.movies">{{ movie.title }}, {{ movie.original_title }}, {{ movie.original_language }}, {{ movie.vote_average }}</li>
+   </ul>
+   
 
 </template>
 
--- src/state.js
+++ src/state.js
@@ -2,5 +2,17 @@ import { reactive } from "vue";
 import axios from "axios";
 
 export const state = reactive({
-   
+   inputSearchMovie: "",
+   movies: "",
+   base_url: "https://api.themoviedb.org/3/search/movie?api_key=2591c3e5f53b06a41e1529a348580043&language=it-IT&query=",
+
+   fetchData(){
+      axios
+      .get(`${this.base_url}${this.inputSearchMovie}`)
+      .then(response => {
+         this.movies = response.data.results;
+      })
+      this.inputSearchMovie = "";
+       
+   }
 })
