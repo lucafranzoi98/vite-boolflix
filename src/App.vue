b2712ef3211917f53d8fe83456c953731cf3ea58
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-29 14:57:47 +0200

    add controls and img
    add controls on field, if is empty,
    add controls on return of empty list of movies or series,
    add img to results and default img if there isn't one

--- src/App.vue
+++ src/App.vue
@@ -13,16 +13,31 @@ export default{
 
 <template>
 
-   <input type="text" placeholder="Search a movie" v-model="state.inputSearchMovie">
+   <input type="text" placeholder="Search a movie" v-model="state.inputSearchMovie" @keyup.enter="state.searchButton()">
    <button @click="state.searchButton()">SEARCH</button>
+   <div class="text-warning" v-if="state.isNotValid == true">Type at least 1 character</div>
    
-   <ul>Movies
-      <li v-for="movie in state.movies">{{ movie.title }}, {{ movie.original_title }}, {{ state.getFlagEmoji(movie.original_language) }}, {{ movie.vote_average }}</li>
-   </ul>
+   <div v-if="state.isEmpty == false">
+      <ul v-if="state.movies != ''">Movies
+         <li v-for="movie in state.movies">{{ movie.title }}, {{ movie.original_title }}, {{ state.getFlagEmoji(movie.original_language) }}, {{ movie.vote_average }}, {{ movie.backdrop_path }}
+         <img v-if="movie.backdrop_path != null" :src="state.getCover(movie.backdrop_path)">
+         <img v-else src="./assets/img/404.png" width="100"></li>
+      </ul>
+      <div v-else>No movies found</div>
 
-   <ul>Series
-      <li v-for="serie in state.series">{{ serie.name }}, {{ serie.original_name }}, {{ state.getFlagEmoji(serie.original_language) }}, {{ serie.vote_average }}</li>
-   </ul>
+      <ul v-if="state.series != ''">Series
+         <li v-for="serie in state.series">{{ serie.name }}, {{ serie.original_name }}, {{ state.getFlagEmoji(serie.original_language) }}, {{ serie.vote_average }}
+         <img v-if="serie.poster_path != null" :src="state.getCover(serie.poster_path)">
+         <img v-else src="./assets/img/404.png" width="100">
+         </li>
+      </ul>
+      <div v-else>No series found</div>
+   </div>
+
+   <div v-else>
+      Welcome! Search something
+   </div>
+   
 
 </template>
 
--- src/state.js
+++ src/state.js
@@ -7,6 +7,8 @@ export const state = reactive({
    endpointSeries: "https://api.themoviedb.org/3/search/tv?api_key=2591c3e5f53b06a41e1529a348580043&language=it-IT&query=",
    movies: "",
    series: "",
+   isEmpty: true,
+   isNotValid: false,
 
    fetchData(link, result){
       axios
@@ -35,8 +37,16 @@ export const state = reactive({
       },
 
    searchButton(){
-      this.fetchDataMovies(this.endpointMovies, this.movies);
-      this.fetchDataSeries(this.endpointSeries, this.series);
+      if (this.inputSearchMovie.split(" ").join("") != "") {
+         this.fetchDataMovies(this.endpointMovies, this.movies);
+         this.fetchDataSeries(this.endpointSeries, this.series);
+         this.inputSearchMovie = "";
+         this.isEmpty = false;
+         this.isNotValid = false;
+      } else {
+         this.isNotValid = true;
+      }
+      
    },
 
    getFlagEmoji(countryCode) {
@@ -45,7 +55,12 @@ export const state = reactive({
         .split('')
         .map(char =>  127397 + char.charCodeAt());
       return String.fromCodePoint(...codePoints);
-   }, 
+   },
+
+   getCover(path){
+      const imgPath = `https://image.tmdb.org/t/p/w342${path}`
+      return imgPath
+   }
 
 
 })
